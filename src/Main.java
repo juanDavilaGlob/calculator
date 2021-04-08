807cf7fdaf67b172fb9b42c243486d1241a315fa
Juan David Davila Mejia <juan.davila@globant.com>
2021-04-07 21:12:14 -0500

    se crea los métodos de suma y resta

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * Suma dos numeros
+     * @param n1
+     * @param n2
+     * @return
+     */
+    private int sum(int n1, int n2){
+        return n1 + n2;
+    }
+
+    /**
+     * Resta dos numeros
+     * @param n1
+     * @param n2
+     * @return
+     */
+    private int sub(int n1, int n2){
+        return n1 - n2;
+    }
 }
